dbad0ac6bd8a555be0675bed6dcedb5c1c8080c6
Albi Fauzi <albifzi@gmail.com>
2021-04-28 01:39:51 +0700

    setup permission

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -5,6 +5,7 @@
         </header>
         <a href="#" class="block text-gray-200 px-2 py-2">Dashboard</a>
     </div>
+    @if(Auth::user()->can('create playlists'))
     <div class="mb-8">
         <header class="font-medium px-2 text-gray-400 uppercase text-xs">
             Playlist
@@ -12,18 +13,34 @@
         <a href="#" class="block text-gray-200 px-2 py-2">Create</a>
         <a href="#" class="block text-gray-200 px-2 py-2">Table</a>
     </div>
+    @endif
+    @if(Auth::user()->can('create tags'))
+        <div class="mb-8">
+            <header class="font-medium px-2 text-gray-400 uppercase text-xs">
+                Tags
+            </header>
+            <a href="#" class="block text-gray-200 px-2 py-2">Create</a>
+            <a href="#" class="block text-gray-200 px-2 py-2">Table</a>
+        </div>
+    @endif
+
+    @can('show users')
+        <div class="mb-8">
+            <header class="font-medium px-2 text-gray-400 uppercase text-xs">
+                Users
+            </header>
+            <a href="#" class="block text-gray-200 px-2 py-2">Create</a>
+            <a href="#" class="block text-gray-200 px-2 py-2">Table</a>
+        </div>
+    @endcan
+
     <div class="mb-8">
-        <header class="font-medium px-2 text-gray-400 uppercase text-xs">
-            Category
-        </header>
-        <a href="#" class="block text-gray-200 px-2 py-2">Create</a>
-        <a href="#" class="block text-gray-200 px-2 py-2">Table</a>
-    </div>
-    <div class="mb-8">
-        <header class="font-medium px-2 text-gray-400 uppercase text-xs">
-            Users
-        </header>
-        <a href="#" class="block text-gray-200 px-2 py-2">Create</a>
-        <a href="#" class="block text-gray-200 px-2 py-2">Table</a>
+
+        <form method="POST" action="{{ route('logout') }}">
+            @csrf
+            <a href="route('logout')" class="block text-gray-200 px-2 py-2" onclick="event.preventDefault();this.closest('form').submit();">
+                {{ __('Log out') }}
+            </a>
+        </form>
     </div>
 </div>
